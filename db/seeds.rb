0fd00d17dd8eb411189cf25de547682c8eeefcb8
secondbaker <secondbaker@gmail.com>
2020-03-12 12:17:32 -0400

    Just messing with seed data, no biggie

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,23 @@ Collectible.destroy_all
 Collection.destroy_all
 
 fish_collection = Collection.create(title: "Fish")
-first_fish = fish_collection.collectibles.create()
-first_fish.collectible_attributes.create(name: "Checkbox", value: "Checked")
-first_fish.collectible_attributes.create(name: "Name", value: "Shark")
-first_fish.collectible_attributes.create(name: "Time", value: "6:00pm - 8:00pm")
-first_fish.collectible_attributes.create(name: "Month Range", value: "August")
-first_fish.collectible_attributes.create(name: "Location", value: "Mountains for some reason")
-first_fish.collectible_attributes.create(name: "Size", value: "Shark sized")
-first_fish.collectible_attributes.create(name: "Rarity", value: "Very rary")
-first_fish.collectible_attributes.create(name: "Value", value: "10000000 bells")
+first_fish = fish_collection.collectibles.create(name: "Shark", thumbnail: "someurl.org/thumb/img.fileextension", complete: false)
+fish_one_atts = first_fish.collectible_attributes;
+fish_one_atts.create(name: "Time", value: "6:00pm - 8:00pm")
+fish_one_atts.create(name: "Month Range", value: "August")
+fish_one_atts.create(name: "Location", value: "Mountains for some reason")
+fish_one_atts.create(name: "Size", value: "Shark sized")
+fish_one_atts.create(name: "Rarity", value: "Very rary")
+fish_one_atts.create(name: "Value", value: "10000000 bells")
+
+second_fish = fish_collection.collectibles.create(name: "Seahorse", thumbnail: "seahorse.com/file.bmp", complete: true)
+fish_two_atts = second_fish.collectible_attributes;
+fish_two_atts.create(name: "Time", value: "6:00pm - 8:00pm")
+fish_two_atts.create(name: "Month Range", value: "August")
+fish_two_atts.create(name: "Location", value: "Mountains for some reason")
+fish_two_atts.create(name: "Size", value: "Seahorse sized")
+fish_two_atts.create(name: "Rarity", value: "Very rary")
+fish_two_atts.create(name: "Value", value: "10000000 bells")
 
 Collection.create(title: "Bugs")
 Collection.find_by(title: "Bugs").collectibles.create().collectible_attributes.create(name: "rarity")
