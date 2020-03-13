b43d14a9f1d2b78025a24387e9f6c6fb1e7aa4d1
secondbaker <secondbaker@gmail.com>
2020-03-12 21:09:14 -0400

    Got the checkbox working in the view

--- db/schema.rb
+++ db/schema.rb
@@ -12,20 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_12_160620) do
 
-  create_table "attributes", force: :cascade do |t|
-    t.string "name"
-    t.integer "type_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["type_id"], name: "index_attributes_on_type_id"
-  end
-
-  create_table "checkboxes", force: :cascade do |t|
-    t.boolean "checked"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "collectible_attributes", force: :cascade do |t|
     t.string "name"
     t.integer "collectible_id", null: false
@@ -51,19 +37,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_160620) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "name_types", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "types", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  add_foreign_key "attributes", "types"
   add_foreign_key "collectible_attributes", "collectibles"
   add_foreign_key "collectibles", "collections"
 end
