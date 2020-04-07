8b9fa1d716c51bafbe4e0be3aa010df610f6d061
abaker93 <anna.baker.design@gmail.com>
2020-04-07 09:32:15 -0400

    index stuff

--- config/routes.rb
+++ config/routes.rb
@@ -30,6 +30,6 @@ Rails.application.routes.draw do
   resources :collectible_attributes
   resources :collectibles
   resources :collections
-  root "home#index"
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
