051a11fd473b7eec88b96715b9897b9e540c6546
abaker93 <anna.baker.design@gmail.com>
2020-04-07 09:27:08 -0400

    setup index page

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,6 @@ Rails.application.routes.draw do
   resources :collectible_attributes
   resources :collectibles
   resources :collections
+  root "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
