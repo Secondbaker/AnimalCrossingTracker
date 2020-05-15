8130a614ccf08ca523e4c349129d234047e3723e
secondbaker <secondbaker@gmail.com>
2020-05-15 11:06:03 -0400

    Also got rid of the users#show route, since that is never needed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users, except: [:index]
+  resources :users, except: [:index, :show]
   resources :number_collectible_attributes
   resources :collectible_attribute_list_items
   resources :collectible_attribute_lists
