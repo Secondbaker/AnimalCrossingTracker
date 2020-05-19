b97120a6492f8a9cd82e1b4aa8a3d723e5bd1ec1
secondbaker <secondbaker@gmail.com>
2020-05-19 13:20:17 -0400

    Added labels to the StringCollectibleAttributes which exist. The CollectibleAttributeList will also need a label, since the StringCollectibleAttributes within it have no relationship to it. I also need to fix that.

---
+++ db/migrate/20200519165418_find_labels.rb
@@ -0,0 +1,24 @@
+class FindLabels < ActiveRecord::Migration[6.0]
+  def up
+    verbose = true
+    StringCollectibleAttribute.all.each do |string_collectible_attribute|
+      if verbose
+        puts 'StringCollectibleAttribute: ' + string_collectible_attribute.id.to_s
+      end
+      if string_collectible_attribute.label == nil
+        if string_collectible_attribute.collectible_attribute != nil
+          if verbose
+            puts 'Adding label: ' + string_collectible_attribute.collectible_attribute.collectible_attribute_type.name
+          end
+          string_collectible_attribute.label = string_collectible_attribute.collectible_attribute.collectible_attribute_type.name
+          string_collectible_attribute.save
+          puts 'Saved'
+        end
+      end
+    end
+  end
+  # We don't really need to undo this on the way down.  
+  # If you keep going down, the column will just be deleted.
+  def down
+  end
+end
