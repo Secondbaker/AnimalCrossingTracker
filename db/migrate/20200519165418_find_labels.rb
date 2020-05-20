e619d43ce66c04434742254506cfe92629789ba7
secondbaker <secondbaker@gmail.com>
2020-05-20 15:15:54 -0400

    I shouldn't have tried saving data in the migrations.

--- db/migrate/20200519165418_find_labels.rb
+++ db/migrate/20200519165418_find_labels.rb
@@ -1,6 +1,6 @@
 class FindLabels < ActiveRecord::Migration[6.0]
   def up
-    
+  end
   # We don't really need to undo this on the way down.  
   # If you keep going down, the column will just be deleted.
   def down
--- db/migrate/20200519174609_find_collectible_attribute_list_labels.rb
+++ db/migrate/20200519174609_find_collectible_attribute_list_labels.rb
@@ -1,17 +1,6 @@
 class FindCollectibleAttributeListLabels < ActiveRecord::Migration[6.0]
   def up
-    verbose = false
-    CollectibleAttributeList.all.each do |collectible_attribute_list|
-      if verbose
-        puts 'CollectibleAttributeList: ' + collectible_attribute_list.id.to_s
-        puts 'Adding label: ' + collectible_attribute_list.collectible_attribute.collectible_attribute_type.name
-      end
-      collectible_attribute_list.label = collectible_attribute_list.collectible_attribute.collectible_attribute_type.name
-      collectible_attribute_list.save
-      if verbose
-        puts 'Saved'
-      end
-    end
+    
   end
   # we don't care if these stick around when we go down a step; they'll be deleted if we go down enough steps
   def down
